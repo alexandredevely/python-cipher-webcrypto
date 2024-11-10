11aa8d7eaf896f9b80814c38d5e282ddc5dd90a2
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-11-10 16:52:01 +0100

    make rsa keys

---
+++ make_key.sh
@@ -0,0 +1,3 @@
+openssl genrsa -out private.pem 2048
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in private.pem -out private.pkcs8.pem
+openssl rsa -in private.pem -RSAPublicKey_out -out rsapublic.pem
